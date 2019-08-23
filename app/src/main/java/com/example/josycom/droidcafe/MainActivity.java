e65a32d507b7c47c40f50ac3fce9f9fb1ba24d00
MayorJay <joseolu4gsm@yahoo.com>
2019-08-23 06:36:18 +0100

    Addition of Options Menu, alert dialog and date picker

--- app/src/main/java/com/example/josycom/droidcafe/MainActivity.java
+++ app/src/main/java/com/example/josycom/droidcafe/MainActivity.java
@@ -2,16 +2,15 @@ package com.example.josycom.droidcafe;
 
 import android.content.Intent;
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.Toast;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "com.example.josycom.droidcafe.extra.MESSAGE";
@@ -38,7 +37,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
+        // Inflate the menu; this adds items to the action bar if it is present
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
@@ -47,21 +46,32 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        // as you specify a parent activity in AndroidManifest.xml
+        switch (item.getItemId()) {
+            case R.id.action_order:
+                displayToast(getString(R.string.action_order_message));
+                Intent intent = new Intent(MainActivity.this, OrderActivity.class);
+                intent.putExtra(EXTRA_MESSAGE, mOrderMessage);
+                startActivity(intent);
+                return true;
+            case R.id.action_status:
+                displayToast(getString(R.string.action_status_message));
+                return true;
+            case R.id.action_favorites:
+                displayToast(getString(R.string.action_favorites_message));
+                return true;
+            case R.id.action_contact:
+                displayToast(getString(R.string.action_favorites_message));
+                return true;
+            default:
         }
-
         return super.onOptionsItemSelected(item);
     }
 
     /**
      * Creates the toast.
      */
-    public void displayToast(String message){
+    public void displayToast(String message) {
         Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
     }
 
--- app/src/main/java/com/example/josycom/droidcafe/OrderActivity.java
+++ app/src/main/java/com/example/josycom/droidcafe/OrderActivity.java
@@ -1,7 +1,5 @@
 package com.example.josycom.droidcafe;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -11,6 +9,9 @@ import android.widget.RadioButton;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
 public class OrderActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
@@ -25,7 +26,7 @@ public class OrderActivity extends AppCompatActivity implements AdapterView.OnIt
         textView.setText(message);
         // Create the spinner
         Spinner spinner = findViewById(R.id.label_spinner);
-        if (spinner != null){
+        if (spinner != null) {
             spinner.setOnItemSelectedListener(this);
         }
         // Create ArrayAdapter using the string array and default spinner layout
@@ -34,39 +35,39 @@ public class OrderActivity extends AppCompatActivity implements AdapterView.OnIt
         // Specify the layout to use when the list of choices appears
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         // Apply the adapter to the spinner
-        if (spinner != null){
+        if (spinner != null) {
             spinner.setAdapter(adapter);
         }
     }
 
     public void onRadioButtonClicked(View view) {
         // Is the button now checked?
-        boolean checked = ((RadioButton)view).isChecked();
+        boolean checked = ((RadioButton) view).isChecked();
 
         // Check which radio button was clicked.
-        switch (view.getId()){
+        switch (view.getId()) {
             case (R.id.sameday):
-                if(checked)
+                if (checked)
                     // Same day service
                     displayToast(getString(R.string.same_day_messenger_service));
-                    break;
+                break;
             case (R.id.nextday):
-                if(checked)
+                if (checked)
                     // Next day delivery
                     displayToast(getString(R.string.next_day_ground_delivery));
-                    break;
+                break;
             case (R.id.pickup):
-                if(checked)
+                if (checked)
                     // Pick up
                     displayToast(getString(R.string.pick_up));
-                    break;
+                break;
             default:
                 // Do nothing
                 break;
         }
     }
 
-    public void displayToast(String message){
+    public void displayToast(String message) {
         Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
     }
 
@@ -79,5 +80,25 @@ public class OrderActivity extends AppCompatActivity implements AdapterView.OnIt
     @Override
     public void onNothingSelected(AdapterView<?> parent) {
 
+    }
+
+    /*
+    Displays the date picker dialog
+     */
+    public void showDatePicker(View view) {
+        DialogFragment dateFragment = new DatePickerFragment();
+        dateFragment.show(getSupportFragmentManager(), "datePicker");
+    }
+
+    /*
+    Converts the date picked by the user into a string
+    And displays it in a Toast
+     */
+    public void processDatePickerResult(int year, int month, int day) {
+        String year_string = Integer.toString(year);
+        String month_string = Integer.toString(month + 1);
+        String day_string = Integer.toString(day);
+        String dateMsg = (month_string + "/" + day_string + "/" + year_string);
+        Toast.makeText(this, " " + dateMsg, Toast.LENGTH_SHORT).show();
     }
 }
